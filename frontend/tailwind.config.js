a5a8f5617ae75e7bd90b27fff235939649b61f34
Miheer <119855608+Miheergautam@users.noreply.github.com>
2024-07-02 00:51:23 +0530

    Updated the Index.css and dependencies

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        rihno: '#343f71',
+        /* rihno: '#343f71',
         twilight: '#e9d8e4',
         pomegranate: '#f34c19',
-        supernova: '#fbc40e',
+        supernova: '#fbc40e', */
+        'soft-green': '#CAE4DB',
+        'golden-yellow': '#DCAE1D',
+        'deep-teal': '#00303F',
+        'mist': '#7A9D96',
       },
     },
   },
