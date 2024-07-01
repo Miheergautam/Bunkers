28eb5faf1f48e58cbf2b84a41b431a61b9f4caf2
Miheer <119855608+Miheergautam@users.noreply.github.com>
2024-07-02 02:28:49 +0530

    "test"

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,10 +7,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        /* rihno: '#343f71',
-        twilight: '#e9d8e4',
-        pomegranate: '#f34c19',
-        supernova: '#fbc40e', */
         'soft-green': '#CAE4DB',
         'golden-yellow': '#DCAE1D',
         'deep-teal': '#00303F',
